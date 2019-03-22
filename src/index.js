dd82f8feedbacf292355b691829cb148ab4a5a7e
Tracy Reith <treith@treith.com>
2019-03-22 15:48:04 -0700

    Connect api key in .env to return data

--- src/index.js
+++ src/index.js
@@ -2,22 +2,29 @@ import $ from 'jquery';
 import './scss/styles.scss';
 import { DoctorList } from './js/project.js';
 
+$(document).ready(function(){
+  $("#find-doctors").submit(function(event) {
+    event.preventDefault();
 const symptom = $("#user-input").val();
 let doctorList = new DoctorList();
-let promise1 = doctorList.getDoctorList(symptom);
+let promise1 = doctorList.getDoctors(symptom);
 let resultList;
 
 promise1.then((response) => {
   let body = JSON.parse(response);
-  resultList = doctorList.getList(body.profile);
+  resultList = doctorList.getList(body.doctors);
   resultList.forEach((doctor) => {
-    $("#doc-list").html(`<li>${doctor}<li>`)
+    $("#doc-list").html(`<li>${doctor}<li>`);
   });
-});
+  }, (error) => {
+    $('.result').html(`<p>Regrettably, there was an error retrieving information: ${error.message}. Please try again.</p>`);
+    }
+  );
 
-$(document).ready(function(){
-  $("#find-doctors").submit(function(event) {
-    event.preventDefault();
+    console.log("fail");
+    console.log(typeof promise1);
+    console.log(promise1);
     console.log(typeof resultList);
+    console.log(resultList);
   });
 });
