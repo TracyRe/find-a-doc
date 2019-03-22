cd57e73b42ff15534333f3e3e07a28a99cf28e24
Tracy Reith <treith@treith.com>
2019-03-22 16:59:53 -0700

    Add incomplete files

--- src/js/project.js
+++ src/js/project.js
@@ -4,7 +4,7 @@ export class DoctorList {
     let Promise = require("es6-promise").Promise;
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=query=sore%20throat&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${symptom}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
@@ -17,13 +17,13 @@ export class DoctorList {
     });
   }
 
-  getList(doctor) {
-  const doctorArray = [];
-  doctor.forEach((doctorArray) => {
-    doctorArray.push(doctor.data[0].profile.last_name);
-  });
-  return doctorArray;
-  }
+  // getList(doctor) {
+  // const doctorArray = [];
+  // doctor.forEach((doctorArray) => {
+  //   doctorArray.push(doctor.data[0].profile.last_name);
+  // });
+  // return doctorArray;
+  // }
 
 }
 
