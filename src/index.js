c8ca8a4acd3ef5e932b1f6197cca42f4abff6023
Tracy Reith <treith@treith.com>
2019-03-22 20:38:22 -0700

    console last name of a single doctor

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
-import { DoctorList } from '.'
+import $ from 'jquery';
+import './scss/styles.scss';
+import { DoctorList } from './js/project.js';
+
+$("document").ready(function() {
+  $("#find-doctors").submit(function(event) {
+    event.preventDefault();
+
+    const symptom = $("user-input").val();
+    let doctorList = new DoctorList();
+    let promise1 = doctorList.getDoctors(symptom);
+    let lastName;
+    let resultList;
+
+    promise1.then((response) => {
+      let body = JSON.parse(response);
+
+      console.log(body.data[0].profile.last_name);
+
+      // function getList(doctor) {
+      //   const doctorArray = [];
+      //   doctor.forEach((doctorArray) => {
+      //     doctorArray.push(doctor.data.profile.last_name);
+      //   });
+      //   return doctorArray;
+      // }
+      // resultList = doctorList.getList(body.data);
+      // resultList.forEach( (lastName) => {
+      //   $(".doc-list").html(`<li>${lastName}</li>`);
+      //
+      // });
+    }, (error) => {
+      $('.result').html(`<p>Regrettably, there was an error retrieving information: ${error.message}. Please try again.</p>`);
+    });
+
+
+  });
+
+   // console.log(index[0]);
+});
--- src/js/project.js
+++ src/js/project.js
@@ -4,7 +4,7 @@ export class DoctorList {
     let Promise = require("es6-promise").Promise;
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${symptom}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=sore%20throat&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
@@ -20,13 +20,9 @@ export class DoctorList {
   // getList(doctor) {
   // const doctorArray = [];
   // doctor.forEach((doctorArray) => {
-  //   doctorArray.push(doctor.data[0].profile.last_name);
+  //   doctorArray.push(doctor.data.profile.last_name);
   // });
   // return doctorArray;
   // }
 
 }
-
-//query=${symptom}
-
-// ${process.exports.apiKey}
